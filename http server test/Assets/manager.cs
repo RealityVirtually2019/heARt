51d2f8fd28356c3b8695fe158875c8a402eac5bb
tusadi <tpurang@yahoo.in>
2019-01-18 17:43:14 -0500

    capture image + server

---
+++ http server test/Assets/manager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class manager : MonoBehaviour {
+
+	//10.189.126.108
+	public Text address;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (uploadPNG());
+		address.text = "starting";
+	}
+
+	void Update(){
+		
+	}
+	
+	// Update is called once per frame
+	IEnumerator uploadPNG () {
+		
+
+		if (Input.GetTouch(0).phase== TouchPhase.Began) {
+			int width = Screen.width;
+			int height = Screen.height;
+
+			Texture2D tex = new Texture2D (width, height, TextureFormat.RGB24, false);
+
+			tex.ReadPixels (new Rect (0, 0, width, height), 0, 0);
+			tex.Apply ();
+			byte[] bytes = tex.EncodeToPNG ();
+
+			WWWForm form = new WWWForm ();
+			form.AddBinaryData ("image", bytes);
+			WWW w = new WWW ("10.189.126.108:8080", form);
+
+			yield return w;
+			if (w.error != null) {
+				address.text = w.ToString();
+			} else {
+				address.text = "Finished uploading";
+			}
+		//	UnityWebRequest www = UnityWebRequest.Put("url"
+
+		}
+	}
+}
