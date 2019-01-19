fa18f10f9dea65d264326d19316eeb1ed136cfc6
Matthew Fisher <matthewafisher@componententity.com>
2019-01-18 19:41:56 -0500

    got django working with ngrok

--- http server test/Assets/manager.cs
+++ http server test/Assets/manager.cs
@@ -4,46 +4,54 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
 
-public class manager : MonoBehaviour {
+public class manager : MonoBehaviour
+{
 
-	//10.189.126.108
-	public Text address;
-	// Use this for initialization
-	void Start () {
-		StartCoroutine (uploadPNG());
-		address.text = "starting";
-	}
+    //10.189.126.108
+    public Text address;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(uploadPNG());
+        address.text = "starting";
+    }
 
-	void Update(){
-		
-	}
-	
-	// Update is called once per frame
-	IEnumerator uploadPNG () {
-		
+    void Update()
+    {
 
-		if (Input.GetTouch(0).phase== TouchPhase.Began) {
-			int width = Screen.width;
-			int height = Screen.height;
+    }
 
-			Texture2D tex = new Texture2D (width, height, TextureFormat.RGB24, false);
+    // Update is called once per frame
+    IEnumerator uploadPNG()
+    {
 
-			tex.ReadPixels (new Rect (0, 0, width, height), 0, 0);
-			tex.Apply ();
-			byte[] bytes = tex.EncodeToPNG ();
 
-			WWWForm form = new WWWForm ();
-			form.AddBinaryData ("image", bytes);
-			WWW w = new WWW ("10.189.126.108:8080", form);
+        if (Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            int width = Screen.width;
+            int height = Screen.height;
 
-			yield return w;
-			if (w.error != null) {
-				address.text = w.ToString();
-			} else {
-				address.text = "Finished uploading";
-			}
-		//	UnityWebRequest www = UnityWebRequest.Put("url"
+            Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
 
-		}
-	}
+            tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+            tex.Apply();
+            byte[] bytes = tex.EncodeToPNG();
+
+            WWWForm form = new WWWForm();
+            form.AddBinaryData("image", bytes);
+            WWW w = new WWW("http://f8907a7b.ngrok.io/facerecognition/", form);
+
+            yield return w;
+            if (w.error != null)
+            {
+                address.text = w.ToString();
+            }
+            else
+            {
+                address.text = "Finished uploading";
+            }
+            //	UnityWebRequest www = UnityWebRequest.Put("url"
+
+        }
+    }
 }
