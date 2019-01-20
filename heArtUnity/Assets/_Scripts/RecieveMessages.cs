6d7c9a6d53712af885cb8d37ca12c8d3d2cdb3c2
Matthew Fisher <matthewafisher@componententity.com>
2019-01-20 09:07:37 -0500

    hello

---
+++ heArtUnity/Assets/_Scripts/RecieveMessages.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Collections;
+
+public class RecieveMessages : MonoBehaviour {
+
+	int receiverPort = 1998;
+
+	public delegate void OnMessageRecieved(byte[] result);
+	public static OnMessageRecieved messageRecieved;
+
+	UdpClient receiver;
+	byte[] currPacket;
+
+	void Start() {
+		// Create UDP client
+		receiver = new UdpClient(receiverPort);
+		receiver.BeginReceive(DataReceived, receiver);
+	}
+
+	void Update(){
+		if (currPacket.Length > 0) {
+			Debug.Log("Message recieved: " + currPacket);
+			messageRecieved.Invoke(currPacket);
+			currPacket = null;
+		}
+	}
+
+	private void OnApplicationQuit() {
+		receiver.Close();
+	}
+
+	// This is called whenever data is received
+	private void DataReceived(IAsyncResult ar) {
+
+		Debug.Log ("bgn gfhtf");
+		UdpClient c = (UdpClient)ar.AsyncState;
+		IPEndPoint receivedIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+		Byte[] receivedBytes = c.EndReceive(ar, ref receivedIpEndPoint);
+
+		//string packet = System.Text.Encoding.UTF8.GetString (receivedBytes, 0, 20);
+		currPacket = receivedBytes;
+
+		// Restart listening for udp data packages
+		c.BeginReceive(DataReceived, ar.AsyncState);
+	}
+}
---
+++ heArtUnity/Assets/_Scripts/SendMessages.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+public class SendMessages : MonoBehaviour {
+
+	const string IP = "255.255.255.255";
+	const int PORT_NUM = 1998;
+
+	public static SendMessages Instance { get; private set; }
+
+	private IPAddress serverAddr;
+	private IPEndPoint endPoint;
+	private Socket sock;
+	private byte[] send_buffer;
+
+	public MenuElement menu;
+
+	private void Awake() {
+		Instance = this;
+	}
+
+	void Start() {
+		//InitSocket();
+		sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+		sock.EnableBroadcast = true;
+		serverAddr = IPAddress.Parse(IP);
+		print(serverAddr);
+		endPoint = new IPEndPoint(serverAddr, PORT_NUM);
+	}
+
+	void OnApplicationQuit() {
+	
+		sock.Dispose ();
+		sock.Close ();
+
+	}
+		
+
+	public void SendPacket(byte[] data) {
+		try {
+			send_buffer = data;
+			sock.SendTo(send_buffer, endPoint);
+		} catch (SocketException s) {
+			Debug.Log(s);
+		}
+		//menu.ButtonPressed (message);
+	}
+}
