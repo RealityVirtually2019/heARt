3817a53966ad3e102823931631d82e1a0cc3c968
tusadi <tpurang@yahoo.in>
2019-01-20 04:52:34 -0500

    Networking fixed
    Using PC to process and transmit data instead of 2-way communication

--- http server test/Assets/manager.cs
+++ http server test/Assets/manager.cs
@@ -8,13 +8,19 @@ public class Manager : MonoBehaviour {
 	public MenuElement web;
 	public Text txt;
 	int index = 0;
+
+	public TextAsset textFile;
+
 	// Use this for initialization
 	//int i=0;
 	void Start () {
-		InvokeRepeating ("test", 5, 5);
+		if (sender) {
+			InvokeRepeating ("test", 5, 5);
+		}
 	}
 
 	void test(){
+		/*
 		int width = Screen.width;
 		int height = Screen.height;
 		Texture2D tex = new Texture2D (width, height, TextureFormat.RGB24, false);
@@ -22,17 +28,25 @@ public class Manager : MonoBehaviour {
 		tex.ReadPixels (new Rect (0, 0, width, height), 0, 0);
 		tex.Apply ();
 		byte[] bytes = tex.EncodeToPNG ();
+		*/
 
-		SendMessages.Instance.SendPacket (bytes);
-		txt.text = (index++).ToString ();
+		//textFile = Resources.Load("C:\\Users\\tpura\\Desktop\\test.txt") as TextAsset;
+		string text = textFile.text;  //this is the content as string
+		Debug.Log(text.Length);
+		if (text.Length > 0) {
+			byte[] byteText = textFile.bytes;  //this is the content as byte array
+
+			Debug.Log (text);
+
+			SendMessages.Instance.SendPacket (byteText);
+			txt.text = (index++).ToString ();
+		}
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (sender) {
-			if (Input.GetTouch (0).phase == TouchPhase.Began) {
-				
-			}
+			test ();
 		} else {
 			RecieveMessages.messageRecieved += MessageRecieved;
 
